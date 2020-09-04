cc9d1b23146798905d7fa737cd5ab86d86bd59b3
Amit Phadke <58125212+amitp13@users.noreply.github.com>
2020-09-03 21:19:12 -0400

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+from scrambler import scrambler as sc
+
+#local file path for input file
+source = r'C:\Users\phadk\Desktop\Work\projects\set_scrambler\data.docx'
+
+#filename to save as
+save_as = "output"
+
+#local file path to save file 
+destination = r"C:\Users\phadk\Desktop\Work\projects\set_scrambler"
+
+sc.scrambler(source,save_as,destination)
